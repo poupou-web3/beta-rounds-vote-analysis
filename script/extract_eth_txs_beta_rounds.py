45b270967f107056a5098d98604e237b49dd935d
Poupou <108153089+poupou-web3@users.noreply.github.com>
2023-05-05 08:38:56 +0200

    first commit

---
+++ script/extract_eth_txs_beta_rounds.py
@@ -0,0 +1,37 @@
+import os
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+from sbscorer.sbdata.FlipsideApi import FlipsideApi
+
+absolute_path = os.fspath(Path.cwd().parent.parent)
+if absolute_path not in sys.path:
+    sys.path.append(absolute_path)
+
+# Set path to data folder
+current_dir = Path(os.getcwd())
+data_dir = os.path.join(current_dir.parent.parent, 'data')
+
+
+
+# read the address from oss grant
+
+api_key = os.environ['FLIPSIDE_API_KEY']
+flipside_api = FlipsideApi(api_key, max_address=50)
+PATH_TO_ADDRESS = "../../data/grants/df_contribution_normalized_OSS.csv"
+PATH_TO_EXPORT = "../../data/transactions/OSS"
+
+df_address = pd.read_csv(PATH_TO_ADDRESS)
+list_unique_address = df_address.address.unique()
+
+flipside_api.extract_transactions(PATH_TO_EXPORT, list_unique_address)
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'ethereum')
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'polygon')
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'avalanche')
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'optimism')
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'gnosis')
+# flipside_api.extract_transactions_net(PATH_TO_EXPORT, list_unique_address, 'arbitrum')
+
+print("End mining transactions")
