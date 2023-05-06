0756216ec7aa2105fbcc01875dfde48d009fc4c2
Poupou <108153089+poupou-web3@users.noreply.github.com>
2023-05-06 08:34:13 +0200

    improve script web3 community tx data

--- script/extract_eth_txs_beta_rounds.py
+++ script/extract_eth_txs_beta_rounds.py
@@ -6,31 +6,37 @@ import pandas as pd
 
 from sbdata.FlipsideApi import FlipsideApi
 
-# absolute_path = os.fspath(Path.cwd().parent.parent)
-# if absolute_path not in sys.path:
-#     sys.path.append(absolute_path)
+FOLDER_NAME = 'community_beta_rounds'
+ROUND_NAME = 'Web3 Community and Education'
+CHAIN = 'ethereum'
 
 # Set path to data folder
 current_dir = Path(os.getcwd())
-PATH_TO_EXPORT = os.path.join(current_dir.parent.parent, 'tx_data', 'community_beta_rounds')
+PATH_TO_EXPORT = os.path.join(current_dir.parent.parent, 'tx_data', FOLDER_NAME)
 DATA_DIR = os.path.join(current_dir.parent.parent, 'data-regen-rangers')
 
-# read the address from oss grant
-
+# Initialize Flipside API
 api_key = os.environ['FLIPSIDE_API_KEY']
-flipside_api = FlipsideApi(api_key, max_address=300)
+flipside_api = FlipsideApi(api_key, max_address=400)
+
+# 0x12BB5bBbFE596dbc489d209299B8302c3300fa40,Web3 Open Source Software Round,58
+# Load data
 PATH_TO_VOTES = os.path.join(DATA_DIR, "beta_round_votes.csv")
 PATH_TO_GRANTS = os.path.join(DATA_DIR, "all-allo-rounds.csv")
 
-# 0x12BB5bBbFE596dbc489d209299B8302c3300fa40,Web3 Open Source Software Round,58
-
 df_votes = pd.read_csv(PATH_TO_VOTES)
 df_grants = pd.read_csv(PATH_TO_GRANTS)
-round_id = df_grants[df_grants['Round name'] == 'Web3 Community and Education']['Round ID'].values[0]
 
+# Lowercase all addresses
+df_grants['Round ID'] = df_grants['Round ID'].str.lower()
+str_columns_votes = ['id', 'transaction', 'projectId', 'roundId', 'voter', 'grantAddress']
+df_votes[str_columns_votes] = df_votes[str_columns_votes].applymap(lambda x: x.lower())
+
+# get unique voter address for the round
+round_id = df_grants[df_grants['Round name'] == ROUND_NAME]['Round ID'].values[0]
 array_unique_address = df_votes[df_votes['roundId'] == round_id]['voter'].unique()
 
-array_unique_address = np.char.lower(array_unique_address.astype(str))
-flipside_api.extract_transactions_net(PATH_TO_EXPORT, array_unique_address, 'ethereum')
+# extract transactions to the path
+flipside_api.extract_transactions_net(PATH_TO_EXPORT, array_unique_address[:10], CHAIN)
 
 print("End mining transactions")
